ab2556a3531964bcc504099c15e54478c078c4cc
Luiz Felipe vulgo Kaoi <kaoizin@gmail.com>
2015-06-30 08:26:40 -0300

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ http.createServer( app ).listen( config.port, function() {
 var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://kaoi7periodo:kaoi7periodo@ds041651.mongolab.com:41651/nodeapppikaoi', function(err){
-//mongoose.connect('mongodb://kaoizin:Felipe16@ds047732.mongolab.com:47732/nodeappkaoipi', function(err){
   if (err){
     console.log('Erro ao conectar no mongodb: '+err);
   }else{
